5fa2b0601e9889f985c89b92702b3195d57972e9
Fatima Carneiro <fatimascarneiro>
2019-09-09 10:50:07 -0300

    Cadastro e repositório provisório enquanto não crio um banco de dados

---
+++ src/main/java/com/github/fatimascarneiro/selecionador/selecionador/model/Categoria.java
@@ -0,0 +1,5 @@
+package com.github.fatimascarneiro.selecionador.selecionador.model;
+
+public enum Categoria {
+    ANIMACAO, SUSPENSE, ACAO, TERROR, COMEDIA, ROMANCE, COMEDIA_ROMANTICA, INDEPENDENTE, FAMILIA, DRAMA, AVENTURA;
+}
