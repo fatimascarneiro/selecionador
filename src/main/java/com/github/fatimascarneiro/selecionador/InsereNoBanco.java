c53b3bdd08994c1ea498272d891d98fd5e9800f1
Fatima Carneiro <fatimascarneiro>
2019-09-13 16:52:40 -0300

    Salvar filme e série

--- src/main/java/com/github/fatimascarneiro/selecionador/InsereNoBanco.java
+++ src/main/java/com/github/fatimascarneiro/selecionador/InsereNoBanco.java
@@ -1,7 +1,9 @@
 package com.github.fatimascarneiro.selecionador;
 
+import com.github.fatimascarneiro.selecionador.model.Filme;
 import com.github.fatimascarneiro.selecionador.model.Genero;
 import com.github.fatimascarneiro.selecionador.model.Plataforma;
+import com.github.fatimascarneiro.selecionador.model.Serie;
 import com.github.fatimascarneiro.selecionador.repository.FilmeRepository;
 import com.github.fatimascarneiro.selecionador.repository.GeneroRepository;
 import com.github.fatimascarneiro.selecionador.repository.PlataformaRepository;
@@ -10,6 +12,10 @@ import com.github.fatimascarneiro.selecionador.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.Year;
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 public class InsereNoBanco implements CommandLineRunner {
@@ -21,6 +27,14 @@ public class InsereNoBanco implements CommandLineRunner {
 
     private Genero drama = new Genero();
     private Genero comedia = new Genero();
+    private Genero romance = new Genero();
+    private Genero acao = new Genero();
+
+    private Set<Genero> generos = new HashSet<>();
+
+    private Filme filme = new Filme("Titanic", "diretor de titanic", Year.of(2000), generos);
+
+    private Serie serie = new Serie("Todo Mundo Odeia o Cris", generos);
 
     private Plataforma plataforma = new Plataforma("Netflix", 123);
 
@@ -44,8 +58,14 @@ public class InsereNoBanco implements CommandLineRunner {
         this.filmeRepository.deleteAll();
 
         this.plataformaRepository.save(plataforma);
+
         this.generoRepository.save(drama);
         this.generoRepository.save(comedia);
+        this.generoRepository.save(romance);
+        this.generoRepository.save(acao);
 
+        this.filmeRepository.save(filme);
+
+        this.serieRepository.save(serie);
     }
 }
